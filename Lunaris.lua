9211337872062428da150847934176506aa6ea20
minh597 <minhdangtran1012@gmail.com>
2025-10-26 10:12:12 +0700

    Update loadstring URLs for farming strategies

--- Lunaris.lua
+++ Lunaris.lua
@@ -3,7 +3,7 @@ local farm = LunarisX.farm
 local strategy = LunarisX.strategy
 
 if farm == "gems" and strategy == "040811crossroads" then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/minh597/Lunaris/refs/heads/main/040811.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/minh597/TDS-strategy/refs/heads/main/040811.lua"))()
 end
 
 if farm == "gum" and strategy == "batmangumfarm" then
